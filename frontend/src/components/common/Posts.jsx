e27a2887d29c4aa87ba2f07cff71acce28d7a76a
Gokul Raj <serajgokul19@gmail.com>
2024-12-12 21:49:43 +0530

    create post

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,9 +1,44 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { POSTS } from "../../utils/db/dummy";
+import { useQueries, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const Posts = () => {
-	const isLoading = true;
+	
+
+	const getPostEndpoint = ()=>{
+		switch(feedType){
+			case "forYou":
+				return "/api/posts/all"
+			case "following":
+				return "api/posts/following"
+			default:
+				return "/api/posts/all"
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndpoint()
+
+	const {data:posts, isLoading, refetch, isRefetching} = useQuery({
+		queryKey:['posts'],
+		queryfn: async()=>{
+			try {
+				const res = await fetch(POST_ENDPOINT)
+				const data =await res.json();
+
+				if(!res.ok){
+					throw new Error(data.error || "something went wrong")
+				}
+				
+			} catch (error) {
+				throw new Error(error)
+			}
+		}
+	})
+	useEffect(()=>{
+		refetch();
+	}, [feedType, refetch])
 
 	return (
 		<>
@@ -14,8 +49,8 @@ const Posts = () => {
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
 					{POSTS.map((post) => (
 						<Post key={post._id} post={post} />
--- frontend/src/pages/home/CreatePost.jsx
+++ frontend/src/pages/home/CreatePost.jsx
@@ -2,6 +2,7 @@ import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
 import { useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
 
 const CreatePost = () => {
 	const [text, setText] = useState("");
@@ -9,16 +10,40 @@ const CreatePost = () => {
 
 	const imgRef = useRef(null);
 
-	const isPending = false;
-	const isError = false;
+	const{data:authUser} = useQuery({queryKey:['authUser']})
+	const queryClient = useQueryClient();
 
-	const data = {
-		profileImg: "/avatars/boy1.png",
-	};
+	const {mutate:createPost, isPending, isError, error} = useMutation({
+		mutation: async ({text, img})=>{
+			try {
+				const res = await fetch("api/posts/create", {
+					method:"POST",
+					headers:{
+						"Content-Type":"application/json",
+					},
+					body: JSON.stringify({text, img})
+				})
+
+				const data = res.json()
+				if (!res.ok){
+					throw new Error(data.error || "something went wrong")
+				}
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess:()=>{
+			setText("")
+			setInterval(null)
+			toast.success("Post created successfully")
+			queryClient.invalidateQueries({"queryKey":['posts']})
+		}
+	})
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert("Post created successfully");
+		CreatePost({text, img})
 	};
 
 	const handleImgChange = (e) => {
@@ -36,7 +61,7 @@ const CreatePost = () => {
 		<div className='flex p-4 items-start gap-4 border-b border-gray-700'>
 			<div className='avatar'>
 				<div className='w-8 rounded-full'>
-					<img src={data.profileImg || "/avatar-placeholder.png"} />
+					<img src={authUser.profileImg || "/avatar-placeholder.png"} />
 				</div>
 			</div>
 			<form className='flex flex-col gap-2 w-full' onSubmit={handleSubmit}>
@@ -72,7 +97,7 @@ const CreatePost = () => {
 						{isPending ? "Posting..." : "Post"}
 					</button>
 				</div>
-				{isError && <div className='text-red-500'>Something went wrong</div>}
+				{isError && <div className='text-red-500'>{error}</div>}
 			</form>
 		</div>
 	);
